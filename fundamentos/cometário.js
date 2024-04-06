e18c2892a1066ee34c0437b29d432362e2a432ae
Milene Gomes <mileneg.dev@gmail.com>
2024-04-06 15:00:35 -0300

    primeiro commit no meus estudos sobre JavaScript

---
+++ fundamentos/cometário.js
@@ -0,0 +1,14 @@
+// cometario de uma linha
+console.log("Linda 1")
+
+/*
+comentário de 
+multiplas linhas
+*/
+console.log("Linha 2")
+
+/*
+* cometári de multiplas
+*
+*/
+console.log("Linha 3")
---
+++ fundamentos/organização.js
@@ -0,0 +1,15 @@
+console.log ("Sentença de código");
+console.log("Sentença de Código")
+
+// pode ou não terminar com ;
+{
+    //bloco de código simbolo de {}
+}
+
+{
+    {
+      // pode ter um bloco dentro do outro
+      console.log("Olá")  
+      console.log("Mundo!")
+    }
+}
