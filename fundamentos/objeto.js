9698116e12c1f662eebf61c3a6145ca3764a62a9
Milene Gomes <mileneg.dev@gmail.com>
2024-04-12 15:38:30 -0300

    Atualizando os estudos sobre JavaScript

---
+++ fundamentos/objeto.js
@@ -0,0 +1,13 @@
+const prod1 = {} // {}: forma literal de criar um ojeto
+prod1.nome = 'Celular'
+prod1.preco = 4998.90
+prod1['Desconto legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa polo',
+    preco: 79.90
+}
+
+console.log(prod2)
